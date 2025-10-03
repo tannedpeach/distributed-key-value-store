3ba9f96feb90b4e82b90542f51474dc836b350d5
tannedpeach <tanisharathod22@gmail.com>
2025-10-03 05:11:23 -0400

    part 2b

--- src/pbservice/client.go
+++ src/pbservice/client.go
@@ -12,6 +12,13 @@ import "time"
 import "crypto/rand"
 import "math/big"
 
+func nrand() int64 {
+	max := big.NewInt(int64(1) << 62)
+	bigx, _ := rand.Int(rand.Reader, max)
+	x := bigx.Int64()
+	return x
+}
+
 var gid = 0
 
 type Clerk struct {
